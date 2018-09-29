ec1856d69a2d9f2f5fd506373dd92edd0ed59fb1
spiderrabiit <john+spiderrabbit@vetsurgeon.org.uk>
2018-09-29 08:49:04 +0100

    Init repo

---
+++ acestream_to_http.py
@@ -0,0 +1,334 @@
+import SimpleHTTPServer, SocketServer, requests, subprocess
+import os, psutil, time, sys, base64
+import hashlib, json
+PORT = "4523"
+SERVER_IP = "127.0.0.1"
+USERNAME = "user"
+PASSWORD = "acestream"
+dir_path = os.path.dirname(os.path.realpath(__file__))+"/www"  #change this to where you want to store files. Must have "listings" and "segements" subdirectories writeable by script
+
+pid_stat_url = None
+temp_stream_saved = False
+file_save_status = []
+key = ""
+streamtimer = 0
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+  def do_AUTHHEAD(self):
+    self.send_response(401)
+    self.send_header('WWW-Authenticate', 'Basic realm=\"acestream_to_http\"')
+    self.send_header('Content-type', 'text/html')
+    self.end_headers()
+  
+  def do_GET(self):
+    global pid_stat_url, temp_stream_saved, file_save_status, key, streamtimer, dir_path
+    path = self.path.split("/")
+    if path[1] != 'segments':#allow non-password access to live stream segments
+      if self.headers.getheader('Authorization') == None:
+        self.do_AUTHHEAD()
+        pass
+      elif self.headers.getheader('Authorization') != "Basic %s" % key:
+        self.do_AUTHHEAD()
+        pass
+      if self.headers.getheader('Authorization') != "Basic %s" % key:
+        return
+      
+    #check if vlc running 
+    vlcrunning = False
+    for process in psutil.process_iter(): 
+      if '/usr/bin/vlc' in process.cmdline() and '--live-caching' in process.cmdline(): 
+        vlcrunning = True
+        vlcrunning_line = process.cmdline()
+    enginerunning = False
+    if "acestreamengine" in (p.name() for p in psutil.process_iter()): 
+      enginerunning=True
+      
+    if path[1] == 'engine':
+      if len(path) == 3:
+        if path[2] == 'start':
+          proc1 = subprocess.Popen(["/snap/bin/acestreamplayer.engine", "--client-console", "--bind-all", "--live-cache-type", "disk", "--live-disk-cache-size", "1000000000"])
+        elif path[2] == 'stop':
+          for process in psutil.process_iter(): 
+            if process.name() == "acestreamengine" or ('/usr/bin/vlc' in process.cmdline() and '--live-caching' in process.cmdline()):
+              process.kill()
+          pid_stat_url = None
+          if os.path.isfile(dir_path+"/listings/LIVE.strm"):
+            os.remove(dir_path+"/listings/LIVE.strm")
+        elif path[2] == 'stopstream':
+          for process in psutil.process_iter(): 
+            if'/usr/bin/vlc' in process.cmdline() and '--live-caching' in process.cmdline():
+              process.kill()
+          if pid_stat_url is not None:
+            #command_url ?method=stop
+            sr = requests.get(json.loads(pid_stat_url[3])['response']['command_url']+"?method=stop")
+            #print sr.text
+          pid_stat_url = None
+          if os.path.isfile(dir_path+"/listings/LIVE.strm"):
+            os.remove(dir_path+"/listings/LIVE.strm")
+
+        time.sleep(5)
+        self.send_response(302)
+        self.send_header('Location',"/")
+        self.end_headers()
+    
+    elif path[1] == 'transcoding' and pid_stat_url is not None:
+      if len(path) == 3:
+        if path[2] == 'start' and len(pid_stat_url)>0:
+          if vlcrunning == False:#only start one instance
+            temp_stream_saved = False
+            f = open(dir_path+"/listings/LIVE.strm", "w")
+            f.write("http://%s/segments/acestream.m3u8" % (SERVER_IP))
+            f.close()
+            subprocess.Popen(["cvlc", "--live-caching", "30000", pid_stat_url[2], "--sout", "#duplicate{dst=std{access=livehttp{seglen=5,delsegs=true,numsegs=20,index="+dir_path+"/segments/acestream.m3u8,index-url=http://"+SERVER_IP+"/segments/stream-########.ts},mux=ts{use-key-frames},dst="+dir_path+"/segments/stream-########.ts},dst=std{access=file,mux=ts,dst='/tmp/acestream.mkv'}}"])
+        elif path[2] == 'stop' and len(pid_stat_url)>0:
+          for process in psutil.process_iter(): #kill acestream engine and vlc
+             if  process.name() == "acestreamengine" or ('/usr/bin/vlc' in process.cmdline() and '--live-caching' in process.cmdline()):
+               process.kill()
+          pid_stat_url = None
+          if os.path.isfile(dir_path+"/listings/LIVE.strm"):
+            os.remove(dir_path+"/listings/LIVE.strm")
+          temp_stream_saved = True
+        time.sleep(5)
+        self.send_response(302)
+        self.send_header('Location',"/")
+        self.end_headers()
+        
+    elif path[1] == 'savefile' and len(path)==3:
+      for f in path[2].split("?")[1].split("&"):
+        if f.split("=")[0] == "savefilename":
+          savefilename = f.split("=")[1]
+      for process in psutil.process_iter(): 
+        if  process.name() == "acestreamengine" or ('/usr/bin/vlc' in process.cmdline() and '--live-caching' in process.cmdline()):
+          process.kill()
+      file_save_status = ["ffmpeg", "-y", "-i", "/tmp/acestream.mkv", "-c:v", "copy", "-c:a", "copy", "-movflags", "faststart", dir_path+"/listings/"+savefilename+".mp4"]
+      subprocess.Popen(file_save_status)
+      pid_stat_url = None
+      temp_stream_saved = False
+      self.send_response(302)
+      self.send_header('Location',"/")
+      self.end_headers()
+    
+    elif path[1] == 'openpid' and len(path)==3 and enginerunning:
+      for process in psutil.process_iter(): #kill any vlc session
+        if  '/usr/bin/vlc' in process.cmdline() and '--live-caching' in process.cmdline():
+          process.kill()
+      pid_stat_url = None
+      stream_pid = False
+      for f in path[2].split("?")[1].split("&"):
+        if f.split("=")[0] == "pid":
+          stream_pid = f.split("=")[1]
+          stream_uid = hashlib.sha1(stream_pid).hexdigest()
+      if stream_pid:
+        r = requests.get('http://127.0.0.1:6878/ace/getstream?format=json&sid={0}&id={1}'.format(stream_uid, stream_pid))
+        pid_stat_url = [stream_pid, json.loads(r.text)['response']['stat_url'], json.loads(r.text)['response']['playback_url'], r.text, stream_pid]
+      
+      #start stream timer
+      streamtimer = time.time()
+      time.sleep(5)
+      self.send_response(302)
+      self.send_header('Location',"/")
+      self.end_headers()
+
+    else:
+      self.send_response(200)
+      self.send_header('Content-type','text/html')
+      self.end_headers()
+      self.wfile.write("""
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body{
+  font-family: Arial;
+  font-size:12px;
+  text-align:center;
+}
+.button {
+    background-color: #4CAF50;
+    border: none; 
+    color: white; 
+    padding: 12px 24px; 
+    font-size: 12px;
+    cursor: pointer;
+    width:210px;
+    text-align:left;
+}
+.buttonloading {
+    background-color: #4CAF50;
+    }
+.buttonstopped{
+    background-color: #AF504C
+    }
+  
+.fa {
+    margin-left: -12px;
+    margin-right: 8px;
+}
+.status{
+    font-family:Courier;
+    font-size:80%;
+}
+.statusdiv{
+    margin:auto;
+    text-align:center;
+    width:400px;
+    padding-top:10px;
+    padding-bottom:20px;
+    font-family: Courier;
+    font-weight: bold;
+}
+</style>
+</head>
+<body>
+""")
+      self.wfile.write("<div>")
+      disabledtext = ["", ""]
+      if enginerunning: 
+        disabledtext[0] = "disabled style='opacity: 0.4;'"
+        engine_status_text = "Running"
+      else: 
+        disabledtext[1] = "disabled style='opacity: 0.4;'"
+        engine_status_text = "Not running"
+      self.wfile.write("""
+        <button class='button buttonloading' %s onclick='this.style.display=\"none\";document.getElementById(\"button_starting\").style.display=\"\";location.href="/engine/start\"'>
+        <i class='fa fa-stop-circle'></i>Start Engine</button>
+       <button id='button_starting' class='button buttonloading' style='display:none'> 
+        <i class='fa fa-spinner fa-spin'></i>Starting Engine</button>
+       """ % disabledtext[0])
+      self.wfile.write("""
+        <button class='button buttonstopped' %s onclick='this.style.display=\"none\";document.getElementById(\"button_stopping\").style.display=\"\";location.href="/engine/stop\"'>
+        <i class='fa fa-play-circle'></i>Stop Engine</button>
+        <button id='button_stopping' class='button buttonloading' style='display:none'> 
+        <i class='fa fa-spinner fa-spin'></i>Stopping Engine</button>
+        """% disabledtext[1])
+      
+      self.wfile.write("</div><div class='statusdiv'>Engine Status: %s</div>" % engine_status_text)
+      
+      disabledtext = ["", ""]
+      stream_OK_to_transcode = False
+      if enginerunning:
+        if pid_stat_url is not None:
+          s = requests.get(pid_stat_url[1])
+          engine_response = json.loads(s.text)
+          disabledtext[0] = "disabled style='opacity: 0.4;'"
+          acestream_status_text = "Streaming<br />acestream://%s<br />" % pid_stat_url[0]
+          if (time.time() - streamtimer) < 30:
+            acestream_status_text+="Wait 30s for stream to settle<br />"
+            acestream_status_text+="""
+            <script>
+            var timeleft = 30;
+            var downloadTimer = setInterval(function(){
+              document.getElementById("progressBar").value = 30 - --timeleft;
+              if(timeleft <= 0){
+                clearInterval(downloadTimer);
+                location.href="/";
+              }
+            },1000);
+            </script>
+            <progress value="0" max="30" id="progressBar"></progress>
+            <br />
+            """
+          elif 'response' in engine_response:
+            if 'speed_down' in engine_response['response']:
+              if engine_response['response']['status'] == "prebuf":
+                acestream_status_text+="Buffering"
+              elif int(engine_response['response']['speed_down']) < 100:
+                acestream_status_text+="Stream health poor"
+              elif int(engine_response['response']['speed_down']) < 300:
+                acestream_status_text+="Stream health average"
+                stream_OK_to_transcode = True
+              else:
+                acestream_status_text+="Stream health OK"
+                stream_OK_to_transcode = True
+              if stream_OK_to_transcode:
+                acestream_status_text+="<br />DL Speed: %s kbps<br />Peers %s" % (engine_response['response']['speed_down'], engine_response['response']['peers'])
+              else:#reload every 5 seconds if buffering or poor stream health
+                self.wfile.write("""<script>
+                  setTimeout(function(){
+                    window.location.reload(1);
+                  }, 5000);
+                  </script>""")
+            
+        else:
+          disabledtext[1] = "disabled style='opacity: 0.4;'"
+          acestream_status_text = "No stream"
+      else:
+        disabledtext[0] = "disabled style='opacity: 0.4;'"
+        disabledtext[1] = "disabled style='opacity: 0.4;'"
+        acestream_status_text = ""
+      self.wfile.write('<div><form method="GET" action="/openpid/" id="openpid" style="display:inline"><input type=text name="pid" size=50 placeholder="Acestream ID" style="margin-bottom:10px;" %s><br>' % disabledtext[0])   
+      self.wfile.write("""
+        <button class='button buttonloading' %s onclick='this.style.display="none"; document.getElementById("button_ace_starting").style.display=""; document.getElementById("openpid").submit()'>
+        <i class='fa fa-stop-circle'></i>Start Acestream</button>
+        <button id='button_ace_starting' class='button buttonloading' style='display:none'>
+        <i class='fa fa-spinner fa-spin'></i>Starting Acestream</button></form>
+      """ % disabledtext[0])
+      self.wfile.write("""
+        <button class='button buttonstopped' %s onclick='this.style.display="none";document.getElementById("button_ace_stopping").style.display="";location.href="/engine/stopstream"'>
+        <i class='fa fa-play-circle'></i>Stop Acestream</button>
+        <button id="button_ace_stopping" class='button buttonstopped' style='display:none'> 
+        <i class='fa fa-spinner fa-spin'></i>Stopping Acestream</button>
+        """ % disabledtext[1])
+      self.wfile.write("</div><div class='statusdiv'>Acestream Status: %s</div>" % acestream_status_text)
+      
+      self.wfile.write("<div>")
+      disabledtext = ["", ""]
+      if stream_OK_to_transcode:
+        if vlcrunning:
+          disabledtext[0] = "disabled style='opacity: 0.4;'"
+          transcode_status_text = """
+          Transcoding<br />Stream: http://%s/segments/acestream.m3u8<br />
+          Kodi: http://%s/listings/LIVE.strm<br />
+          """ % (SERVER_IP, SERVER_IP)
+        else:
+          disabledtext[1] = "disabled style='opacity: 0.4;'"
+          transcode_status_text = "Not Transcoding"
+      else:
+        disabledtext[0] = "disabled style='opacity: 0.4;'"
+        disabledtext[1] = "disabled style='opacity: 0.4;'"
+        transcode_status_text = ""
+      self.wfile.write("""
+        <button class='button buttonloading' %s onclick='this.style.display=\"none\";document.getElementById(\"transcode_starting\").style.display=\"\";location.href="/transcoding/start\"'>
+        <i class='fa fa-stop-circle'></i>Start Transcode</button>
+       <button id='transcode_starting' class='button buttonloading' style='display:none'> 
+        <i class='fa fa-spinner fa-spin'></i>Starting Transcode</button>
+       """ % disabledtext[0])
+      self.wfile.write("""
+        <button class='button buttonstopped' %s onclick='this.style.display=\"none\";document.getElementById(\"transcode_stopping\").style.display=\"\";location.href="/transcoding/stop\"'>
+        <i class='fa fa-play-circle'></i>Stop Transcode</button>
+        <button id='transcode_stopping' class='button buttonloading' style='display:none'> 
+        <i class='fa fa-spinner fa-spin'></i>Stopping Trancode</button>
+        """% disabledtext[1])
+      
+      self.wfile.write("</div><div class='statusdiv'>Transcode Status: %s</div>" % transcode_status_text) 
+
+      if vlcrunning: disabledtext[0] = ""
+      else: disabledtext[0] = "disabled style='opacity: 0.4;'"
+      self.wfile.write("<button class='button buttonloading' %s onclick='window.open(\"http://%s/player.html\", \"\", \"width=660,height=380\");'><i class='fa fa-play-circle'></i>Launch Player</button>" % (disabledtext[0], SERVER_IP))
+      
+      if not temp_stream_saved:  disabledtext[0] = "disabled style='opacity: 0.4;'"
+      else: disabledtext[0] = ""
+      self.wfile.write('''
+       <div>
+       <form method="GET" action="/savefile/" id="savefile">
+       <input type=text name="savefilename" size=50 placeholder="Save filename" style="margin-top:20px; margin-bottom:10px;" %s><br />
+       <button class='button buttonloading' %s onclick='document.getElementById("savefile").submit()'>
+       <i class='fa fa-save'></i>Save Recording</button>
+       </form>
+       </div>
+      ''' % (disabledtext[0], disabledtext[0]))
+      
+
+def main():
+  global key, USERNAME, PASSWORD
+  key = base64.b64encode("%s:%s" % (USERNAME, PASSWORD))
+  SocketServer.TCPServer.allow_reuse_address = True
+  httpd = SocketServer.TCPServer(("", int(PORT)), Handler)
+  print "Running on http://%s:%s" % (SERVER_IP, PORT)
+  httpd.serve_forever()
+
+main()
---
+++ server_install.sh
@@ -0,0 +1,102 @@
+#GENERAL INSTALL INSTRUCTIONS
+#copy/paste and run the line below as root user in the ssh client (without the initial hash/ pound!):
+#bash $(curl -s https://raw.githubusercontent.com/spiderrabbit/acestream-to-http/master/server_install.sh)
+
+if [ -x $(id -u acestream) ] ; then 
+  echo -n "Enter system acestream user password: "
+  read -s password
+  echo
+  echo -n "Re-enter system acestream user password: "
+  read -s chkpassword
+  echo
+  if [ "$password" != "$chkpassword" ]; then
+    echo "Passwords not same. Re-run script"
+    exit
+  fi
+  useradd -d /home/acestream -s /bin/bash -p $(echo $password | openssl passwd -1 -stdin) acestream
+  mkdir /home/acestream
+  cp -rT /etc/skel /home/acestream
+  chown -R acestream:acestream /home/acestream
+fi
+
+serverip=$(curl http://ipinfo.io/ip)
+echo -n "Server IP address [$serverip]: "
+read serverip_temp
+if [ -n "$serverip_temp" ] ; then serverip=$serverip_temp ; fi
+port=4523
+echo -n "Server port to run on [$port]: "
+read port_temp
+if [ -n "$port_temp" ] ; then port=$port_temp ; fi
+webusername="user"
+echo -n "Web username [$webusername]: "
+read webusername_temp
+if [ -n "$webusername_temp" ] ; then webusername=$webusername_temp ; fi
+webpassword="acestream"
+echo -n "Web password [$webpassword]: "
+read webpassword_temp
+if [ -n "$webpassword_temp" ] ; then webpassword=$webpassword_temp ; fi
+
+apt update
+apt install -y vlc ffmpeg python-pip curl nginx
+pip install requests psutil
+snap install acestreamplayer
+
+sudo -u acestream mkdir -p /home/acestream/acestream_to_http/www/listings/
+sudo -u acestream mkdir -p /home/acestream/acestream_to_http/www/segments/
+sudo -u acestream echo "  
+<html>
+<head>
+<title>Acestream Player</title>
+<link href='https://unpkg.com/video.js/dist/video-js.css' rel='stylesheet'>
+<script src='https://unpkg.com/video.js/dist/video.js'></script>
+<script src='https://unpkg.com/videojs-contrib-hls/dist/videojs-contrib-hls.js'></script>
+</head>
+<body>
+<video style='margin:auto' id='my_video_1' class='video-js vjs-default-skin' controls preload='auto' width='640' height='360' data-setup='{}'></video>
+<script>
+const player = videojs('my_video_1');
+player.src({
+src: '/segments/acestream.m3u8',
+type: 'application/x-mpegURL'
+});
+</script>
+</body>
+</html>
+" > /home/acestream/acestream_to_http/www/player.html 
+sudo -u acestream wget https://pastebin.com/raw/T2DMdDRj -O /home/acestream/acestream_to_http/acestream_to_http.py
+sed -i "s/SERVER_IP = \"127.0.0.1\"/SERVER_IP = \"$serverip\"/g" /home/acestream/acestream_to_http/acestream_to_http.py
+sed -i "s/PORT = \"4523\"/PORT = \"$port\"/g" /home/acestream/acestream_to_http/acestream_to_http.py
+sed -i "s/USERNAME = \"user\"/USERNAME = \"$webusername\"/g" /home/acestream/acestream_to_http/acestream_to_http.py
+sed -i "s/PASSWORD = \"acestream\"/PASSWORD = \"$webpassword\"/g" /home/acestream/acestream_to_http/acestream_to_http.py
+
+echo "[Unit]
+Description=Acestream Service
+After=multi-user.target
+Conflicts=getty@tty1.service
+
+[Service]
+Type=simple
+User=acestream
+ExecStart=/usr/bin/python /home/acestream/acestream_to_http/acestream_to_http.py
+
+[Install]
+WantedBy=multi-user.target" > /lib/systemd/system/acestream_to_http.service
+
+echo "server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	root /home/acestream/acestream_to_http/www/;
+	index index.html index.htm index.nginx-debian.html;
+	server_name _;
+	location / {
+               autoindex on;
+	}
+}" > /etc/nginx/sites-enabled/default
+service nginx restart
+
+systemctl daemon-reload
+systemctl enable acestream_to_http.service
+systemctl stop acestream_to_http.service
+systemctl start acestream_to_http.service
+
+echo server running at http://$serverip:$port , login $webusername:$webpassword
