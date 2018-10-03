0b524f2f51682c50c154dd40cfd11b759ee36534
john <john+spiderrabbit@vetsurgoen.org.uk>
2018-10-03 09:47:25 +0100

    updated install flow

--- server_install.sh
+++ server_install.sh
@@ -37,61 +37,21 @@ read webpassword_temp
 if [ -n "$webpassword_temp" ] ; then webpassword=$webpassword_temp ; fi
 
 apt update
-apt install -y vlc ffmpeg python-pip curl nginx
+apt install -y vlc ffmpeg python-pip curl nginx unzip
 pip install requests psutil
 snap install acestreamplayer
 
-sudo -u acestream mkdir -p /home/acestream/acestream_to_http/www/listings/
-sudo -u acestream mkdir -p /home/acestream/acestream_to_http/www/segments/
-sudo -u acestream echo "  
-<html>
-<head>
-<title>Acestream Player</title>
-<link href='https://unpkg.com/video.js/dist/video-js.css' rel='stylesheet'>
-<script src='https://unpkg.com/video.js/dist/video.js'></script>
-<script src='https://unpkg.com/videojs-contrib-hls/dist/videojs-contrib-hls.js'></script>
-</head>
-<body>
-<video style='margin:auto' id='my_video_1' class='video-js vjs-default-skin' controls preload='auto' width='640' height='360' data-setup='{}'></video>
-<script>
-const player = videojs('my_video_1');
-player.src({
-src: '/segments/acestream.m3u8',
-type: 'application/x-mpegURL'
-});
-</script>
-</body>
-</html>
-" > /home/acestream/acestream_to_http/www/player.html 
-sudo -u acestream wget https://raw.githubusercontent.com/spiderrabbit/acestream-to-http/master/acestream_to_http.py -O /home/acestream/acestream_to_http/acestream_to_http.py
-sed -i "s/SERVER_IP = \"127.0.0.1\"/SERVER_IP = \"$serverip\"/g" /home/acestream/acestream_to_http/acestream_to_http.py
-sed -i "s/PORT = \"4523\"/PORT = \"$port\"/g" /home/acestream/acestream_to_http/acestream_to_http.py
-sed -i "s/USERNAME = \"user\"/USERNAME = \"$webusername\"/g" /home/acestream/acestream_to_http/acestream_to_http.py
-sed -i "s/PASSWORD = \"acestream\"/PASSWORD = \"$webpassword\"/g" /home/acestream/acestream_to_http/acestream_to_http.py
+sudo -u acestream wget https://github.com/spiderrabbit/acestream-to-http/archive/master.zip -O /tmp/master.zip
+sudo -u acestream unzip /tmp/master.zip -d /home/acestream/
 
-echo "[Unit]
-Description=Acestream Service
-After=multi-user.target
-Conflicts=getty@tty1.service
+sed -i "s/SERVER_IP = \"127.0.0.1\"/SERVER_IP = \"$serverip\"/g" /home/acestream/acestream-to-http-master/acestream_to_http.py
+sed -i "s/PORT = \"4523\"/PORT = \"$port\"/g" /home/acestream/acestream-to-http-master/acestream_to_http.py
+sed -i "s/USERNAME = \"user\"/USERNAME = \"$webusername\"/g" /home/acestream/acestream-to-http-master/acestream_to_http.py
+sed -i "s/PASSWORD = \"acestream\"/PASSWORD = \"$webpassword\"/g" /home/acestream/acestream-to-http-master/acestream_to_http.py
 
-[Service]
-Type=simple
-User=acestream
-ExecStart=/usr/bin/python /home/acestream/acestream_to_http/acestream_to_http.py
+cp /home/acestream/acestream-to-http-master/conf/acestream_to_http.service > /lib/systemd/system/acestream_to_http.service
 
-[Install]
-WantedBy=multi-user.target" > /lib/systemd/system/acestream_to_http.service
-
-echo "server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	root /home/acestream/acestream_to_http/www/;
-	index index.html index.htm index.nginx-debian.html;
-	server_name _;
-	location / {
-               autoindex on;
-	}
-}" > /etc/nginx/sites-enabled/default
+cp /home/acestream/acestream-to-http-master/conf/nginx.conf /etc/nginx/sites-enabled/default
 service nginx restart
 
 systemctl daemon-reload
@@ -99,4 +59,4 @@ systemctl enable acestream_to_http.service
 systemctl stop acestream_to_http.service
 systemctl start acestream_to_http.service
 
-echo server running at http://$serverip:$port , login $webusername:$webpassword
+echo server running at http://$serverip:$port (login $webusername:$webpassword)
