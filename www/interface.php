9c648acc37e12adc7b6fff0bbde680da7077fe34
Your Name <you@example.com>
2018-10-25 21:27:26 +0100

    add interface

--- www/interface.php
+++ www/interface.php
@@ -1,6 +1,7 @@
 <?php
 if($_GET['action']=='record'){
     $torecord = json_decode('../torecord.json',1);
+    print_r($torecord);
     $torecord[] = $_GET['name'];
     file_put_contents('../torecord.json', json_encode($torecord));
 }
