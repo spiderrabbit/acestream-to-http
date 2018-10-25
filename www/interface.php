bd42dceffae741c842594ea79aae20ae04f485c6
Your Name <you@example.com>
2018-10-25 21:24:41 +0000

    initial interface

--- www/interface.php
+++ www/interface.php
@@ -3,5 +3,5 @@ if($_GET['action']=='record'){
     $torecord = json_decode(file_get_contents('../torecord.json'),1);
     $torecord[] = $_GET['name'];
     file_put_contents('../torecord.json', json_encode($torecord));
-    echo "OK"
+    echo "OK";
 }
