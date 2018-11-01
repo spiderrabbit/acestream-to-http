9a51de0f1f194fbf6ed6c63435eceba890a41500
Your Name <you@example.com>
2018-11-01 17:54:05 +0000

    update live streaming

--- playstream.py
+++ playstream.py
@@ -31,14 +31,17 @@ else:
   stream_uid = hashlib.sha1(stream_pid).hexdigest()
   r = requests.get('http://127.0.0.1:6878/ace/getstream?format=json&sid={0}&id={1}'.format(stream_uid, stream_pid))
   out = json.loads(r.text)
-  out['response']['stream_pid'] = stream_pid
-  #pid_stat_url = [stream_pid, json.loads(r.text)['response']['stat_url'], json.loads(r.text)['response']['playback_url'], r.text, stream_pid]
-  with open('/tmp/pid_stat_url', 'w') as f: f.write(json.dumps(out))
+  if out['response'] is not None:
+    out['response']['stream_pid'] = stream_pid
+    #pid_stat_url = [stream_pid, json.loads(r.text)['response']['stat_url'], json.loads(r.text)['response']['playback_url'], r.text, stream_pid]
+    with open('/tmp/pid_stat_url', 'w') as f: f.write(json.dumps(out))
 
-  time.sleep(5)
+    time.sleep(5)
 
-  acestream_to_http_tc.startvlc(dir_path, PROTOCOL, USERNAME, PASSWORD, SERVER_IP)
+    acestream_to_http_tc.startvlc(dir_path, PROTOCOL, USERNAME, PASSWORD, SERVER_IP)
 
-  print '{0}://{1}:{2}@{3}/segments/acestream.m3u8'.format(SERVER_IP, USERNAME, PASSWORD, SERVER_IP)
+    print '{0}://{1}:{2}@{3}/segments/acestream.m3u8'.format(PROTOCOL, USERNAME, PASSWORD, SERVER_IP)
+  else:
+    print out['error']
 
   #speed_down is in kilobytes / sec
