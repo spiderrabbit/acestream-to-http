81f0905180dccba0cb7b098d3cde9c6c2d0df2ce
Your Name <you@example.com>
2018-12-27 17:56:59 +0000

    add interface functionality

--- www/interface.php
+++ www/interface.php
@@ -49,30 +49,49 @@ else if($_GET['action']=='status'){
   if (strlen($current_recording)>0){
     $status = ['current_recording'=>$current_recording];
   }
+  else{
+    $status = ['current_recording'=>''];
+  }
+  $status['ip'] = @file_get_contents("http://ipinfo.io/ip");
+  $status['diskfreespace'] = disk_free_space("/home/acestream");
   echo json_encode($status);
 }
 else if($_GET['action']=='listings'){
-  #echo file_get_contents('listings/recordings.json');
-  ?>
-<rss version="2.0">
-  <channel>
-    <title>Karaoke</title>
-    <link>http://vk-karaoke.appspot.com/rss</link>
-    <description>https://vk.com/vkkar</description>
-    <lastBuildDate>Thu, 23 Jun 2016 16:00:00 GMT</lastBuildDate>
-    <generator>vkkar</generator>
-    <ttl>3600</ttl>
-    <item>
-      <title>test.mp3</title>
-      <link>http://vk-karaoke.appspot.com/u/140%20%D0%A3%D0%B4%D0%B0%D1%80%D0%BE%D0%B2%20%D0%92%20%D0%9C%D0%B8%D0%BD%D1%83%D1%82%D1%83%20-%20%D0%9E%D0%B9%2C%20%D0%9E%D0%B9.mp3</link>
-      <guid isPermaLink="true">344191188</guid>
-      <pubDate>Thu, 23 Jun 2016 16:00:09 GMT</pubDate>
-      <enclosure url="http://cs611223.vk.me/u9488564/audios/d16a787f7d21.mp3?extra=uRsIn3dT8ffJYzFYpd-SkROo9MYP0huTsFTRlxIo70VAer3B4LurmEXPLsrdP_b9DdEKVU15nD1xL5jYBOm3pyN7tF31UysfGZ5bRzlriMzrI_iQt0QhRWynCdeMeCve7V0SQCPL2_Ejiw" length="0" type="audio/mpeg"/>
-    </item>
-  </channel>
-</rss>
-<?php
+  echo file_get_contents('listings/recordings.json');
 }
+else if($_GET['action']=='deleterecordedfile'){
+  //delete recording + transcoded 
+  $recorded = json_decode(file_get_contents('listings/recordings.json'),1);
+  $matchname = $recorded[$_GET['recording']];
+  $transcodedmatchname = "TRANSCODED_".$matchname;
+  $transcodedkey = array_search($transcodedmatchname, $recorded);
+  unlink('listings/'.$matchname.'.mp4');
+  unlink('listings/'.$transcodedmatchname.'.mp4');
+  unlink('store/'.$_GET['recording'].'.mp4');
+  unlink('store/'.$transcodedkey.'.mp4');
+  //rebuild recordings json
+  unset($recorded[$_GET['recording']]);
+  unset($recorded[$transcodedkey]);
+  file_put_contents('listings/recordings.json', json_encode($recorded));
+}
+// <rss version="2.0">
+//   <channel>
+//     <title>Karaoke</title>
+//     <link>http://vk-karaoke.appspot.com/rss</link>
+//     <description>https://vk.com/vkkar</description>
+//     <lastBuildDate>Thu, 23 Jun 2016 16:00:00 GMT</lastBuildDate>
+//     <generator>vkkar</generator>
+//     <ttl>3600</ttl>
+//     <item>
+//       <title>test.mp3</title>
+//       <link>http://vk-karaoke.appspot.com/u/140%20%D0%A3%D0%B4%D0%B0%D1%80%D0%BE%D0%B2%20%D0%92%20%D0%9C%D0%B8%D0%BD%D1%83%D1%82%D1%83%20-%20%D0%9E%D0%B9%2C%20%D0%9E%D0%B9.mp3</link>
+//       <guid isPermaLink="true">344191188</guid>
+//       <pubDate>Thu, 23 Jun 2016 16:00:09 GMT</pubDate>
+//       <enclosure url="http://cs611223.vk.me/u9488564/audios/d16a787f7d21.mp3?extra=uRsIn3dT8ffJYzFYpd-SkROo9MYP0huTsFTRlxIo70VAer3B4LurmEXPLsrdP_b9DdEKVU15nD1xL5jYBOm3pyN7tF31UysfGZ5bRzlriMzrI_iQt0QhRWynCdeMeCve7V0SQCPL2_Ejiw" length="0" type="audio/mpeg"/>
+//     </item>
+//   </channel>
+// </rss>
+
 // else if($_GET['action']=='listings'){
 //   $recordings = json_decode(file_get_contents('listings/recordings.json'),1);
 //   print_r($recordings);
@@ -91,3 +110,5 @@ else if($_GET['action']=='listings'){
 //   }
 //   print_r($outdirlist);
 // }
+
+?>
